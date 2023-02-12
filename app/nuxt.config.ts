61a688bc0be0c42008efaac15aaf15c1ea64f8ed
daisuke saito <higashiishiyama.5983@gmail.com>
2023-02-12 14:26:29 +0900

    set meta tag

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: true,
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
   css: [
     '@/assets/styles/tailwind.css',
@@ -11,6 +12,15 @@ export default defineNuxtConfig({
   // plugins: ['@/plugins/fontawesome.ts'],
   app: {
     head: {
+      title: "なにたべ",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "apple-touch-icon", sizes: "152x152", href: "/apple-touch-icon.png" },
+      ],
       htmlAttrs: {
         "data-theme": "retro",
       },
