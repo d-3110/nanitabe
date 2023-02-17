fedcdbcf6737b49c45456452c45b2562cc950f21
daisuke saito <higashiishiyama.5983@gmail.com>
2023-02-18 01:13:11 +0900

    fix

--- app/components/Container.vue
+++ app/components/Container.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div class="px-4 py-6 sm:px-0">
+      <div class="px-2 pt-6 pb-14 sm:px-0">
         <slot />
       </div>
     </div>
